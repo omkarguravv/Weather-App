a3ac31f22a5a856381b0f0e4c816808ac8b785ce
OmkarGurav <omkargurav1920@gmail.com>
2022-08-20 16:59:51 +0530

    Weather-App implemented successfully

---
+++ src/Components/WheatherApp.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import './WheatherApp.css';
+
+const WheatherApp=() =>{
+
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState("mumbai");
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=3b7bf1fda138ddf8787b867e51d5b9a7`
+
+            const response = await fetch(url);
+            // console.log(response);
+            const resJson = await response.json();
+            // console.log(resJson);
+            setCity(resJson.main);
+        };
+        fetchApi();
+    }, [search])
+
+    return (
+        <>
+
+            <div className="box">
+                <div className="inputData">
+                    <input type="search" className='inputField'
+                    value={search}
+                        onChange={(e) => {
+                            setSearch(e.target.value)
+                        }}
+                    />
+                </div>
+
+                {!city ?
+                    (<p className="errorMsg">No Data found</p>)
+                    :
+                    (
+                        <div >
+
+                            <div className="info">
+                                <h2 className="location">
+                                    <i className="fas fa-street-view"></i>{search}
+                                </h2>
+
+                                <h1 className="tempt">
+                                    {city.temp} °cel
+                                </h1>
+
+                                <h3 className="tempmin_max">
+                                  Min :  {city.temp_min} °cel  
+                                  <br />
+                                  Max :  {city.temp_max} °cel
+                                </h3>
+                            </div>
+
+                            <div className="wave -one"></div>
+                            <div className="wave -two"></div>
+                            <div className="wave -three"></div>
+                        </div>
+                        
+                    )}
+
+
+
+            </div>
+
+
+        </>
+    )
+}
+
+export default WheatherApp;
